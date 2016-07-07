4dfffaee1ab500880066cc57fdeb4d421c335ceb
manish9857 <manish9857@gmail.com>
2016-07-08 03:35:41 +0530

    Update Week 3 assignment.R

--- Week 3 assignment.R
+++ Week 3 assignment.R
@@ -1,3 +1,7 @@
+## A function to create a matrix and another function to cache the inverse of that matrix
+
+## The following function is to set and get a new matrix and inverse matrix
+## the inverse matrix may or may not the inverse of the actual matrix 
 makeCacheMatrix<-function(x=matrix()){
 	inv<-NULL
 	set<-function(y){
@@ -9,6 +13,9 @@ makeCacheMatrix<-function(x=matrix()){
 	getinv<-function() inv
 	list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
+
+## The following function is to cache the inverse of matrix created above 
+## if the inverse matrix is not set then it calculates the inverse of actual matrix
 cacheSolve<-function(x,...){
 	inv<-x$getinv()
 	if(!is.null(inv)){
